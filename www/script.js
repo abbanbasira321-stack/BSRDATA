4ee1841fcbfb92fe9e17fb27d9be146d8a7528ad
Yahaya Muazu <yahayaabbanbasira321@gmail.com>
2026-08-05 12:55:14 +0100

    First BSR DATA commit

---
+++ www/script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll(".card").forEach(card => {
+  card.addEventListener("click", () => {
+    const service = card.querySelector("h3").textContent;
+    alert("Ka zaɓi: " + service);
+  });
+});
+
+document.querySelector(".wallet button").addEventListener("click", () => {
+  alert("Fund Wallet feature yana zuwa nan ba da jimawa ba.");
+});
